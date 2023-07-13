82de6dee289746bc1754768a826958cf4321fe83
Pavel-Baranenko <baranenko_work@mail.ru>
2023-07-13 20:40:17 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -171,30 +171,7 @@ cookie.addEventListener("click", () => {
 
 
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelectorAll(".trigger");
-var closeButton = document.querySelectorAll(".close-button");
 
-var modalVert = document.querySelector(".modal-vert");
-var triggerVert = document.querySelectorAll(".trigger-vert");
-
-function toggleModal() {
-   modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-   if (event.target === modal) {
-      toggleModal();
-   }
-}
-
-trigger.forEach(function (item) {
-   item.addEventListener('click', toggleModal);
-});
-closeButton.forEach(function (item) {
-   item.addEventListener('click', toggleModal);
-});
-window.addEventListener("click", windowOnClick);
 
 
 
