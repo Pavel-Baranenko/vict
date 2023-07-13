d57847349bb2fcd23449e461be9c0bde84b25ba4
Pavel-Baranenko <baranenko_work@mail.ru>
2023-07-13 11:01:14 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -129,22 +129,6 @@ var swiperNew = new Swiper(".swiper-art", {
    },
 });
 
-var element = document.getElementById('overlay');
-
-const link = document.querySelector('.hero__links');
-const music = document.querySelector('.music');
-
-window.addEventListener('scroll', function () {
-   if (window.scrollY > 500) {
-      element.classList.add("shadow");
-      music.classList.add("shadow");
-      link.classList.add("active");
-   } else {
-      element.classList.remove("shadow");
-      music.classList.remove("shadow");
-      link.classList.remove("active");
-   }
-});
 
 
 const tabsBtn = document.querySelectorAll(".tab-btn");
